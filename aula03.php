9aadb9819be74370449acf387fd66985bec6cc1e
lilizz9 <lilianbarbosa421@gmail.com>
2024-07-29 14:18:00 -0300

    Add files via upload

---
+++ aula03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doc</title>
+</head>
+<body>
+
+<?php
+for ($x=1; $x<=10; $x++){
+echo "$x <br>";
+};
+
+
+echo"<h1> Tabela de 1 a 10 em PHP </h1>";
+
+
+?>
+    
+</body>
+</html>
