f8d7381912f19c8ab900bfe5c8fa6169eec0bbaf
lilizz9 <lilianbarbosa421@gmail.com>
2024-08-05 18:21:30 -0300

    Add files via upload

---
+++ Aula07.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+<style>
+body
+    {
+
+background-color: lightskyblue;
+    }
+</style>
+
+
+</head>
+<body>
+    <a href="recebe-aula07.php?nome=Lílian"> Enviar o nome aqui</a>
+    <form action="recebe-aula07.php">
+        <label for="nome">Nome: </label>
+        <input type="text" name="nome"><br>
+        <label for="sobrenome">Sobrenome: </label><br>
+        <input type="text" name="sobrenome"><br>
+        <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
